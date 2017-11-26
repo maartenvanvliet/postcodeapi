7d37874982a778a61a8b7f69596c0a74a91c0c44
Maarten van Vliet <maartenvanvliet@gmail.com>
2017-11-26 21:38:23 +0100

    Set package fields

--- mix.exs
+++ mix.exs
@@ -5,9 +5,11 @@ defmodule PostcodeApi.MixProject do
     [
       app: :postcodeapi,
       version: "0.1.0",
-      elixir: "~> 1.6-dev",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      package: package(),
+      description: description()
     ]
   end
 
@@ -18,11 +20,24 @@ defmodule PostcodeApi.MixProject do
     ]
   end
 
+  defp description() do
+    "Simple api client for https://www.postcodeapi.nu"
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:httpoison, "~> 0.13"},
-      {:poison, "~> 3.1"}
+      {:poison, "~> 3.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Maarten van Vliet"],
+      links: %{}
     ]
   end
 end
